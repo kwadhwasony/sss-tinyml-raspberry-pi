73aeb2638b8e8c73150c3c4e81867cd81c77be66
Kunal Wadhwa <kunal.wadhwa@sony.com>
2023-03-02 00:57:14 -0800

    cleaning up code, removing/commenting some extras

--- pythonsrc/demo.py
+++ pythonsrc/demo.py
@@ -16,10 +16,11 @@ import numpy as np
 picam = {}
 runner = None
 azure_client = None
+# connection string pointing towards tanmoys iot hub device
 #CONNECTION_STRING = "HostName=sonydemoiothub.azure-devices.net;DeviceId=sonytestdevice;SharedAccessKey=p/c31bhBeVelkeYLcx7Bws5BY5tBYXym3v1Tg7xTK/U="
+# connection string pointing towards iot hub device on kunal.wadhwa@sony.com account
 CONNECTION_STRING = "HostName=testhubname0.azure-devices.net;DeviceId=RPiDevice0;SharedAccessKey=hv82yeuIj9GTTXnAhuw8HWk7NGUOp9NU4NO/0NOMb9E="
 PAYLOAD = '{{"X": {x_start}, "Y": {y_start}, "x":{height}, "y":{width}}}'
-
 
 # if you don't want to see a camera preview, set this to False
 show_camera = True
@@ -27,22 +28,22 @@ if (sys.platform == 'linux' and not os.environ.get('DISPLAY')):
     show_camera = False
 
 # method to convert rgba to rgb
-# todo: check if we can actually use rgba directly with edge impulse sample code
-def rgba2rgb( rgba, background=(255,255,255) ):
+# note1: this method only does matrix transforms and does not indeed check
+#	if the input is indeed a valid rgb format value
+# note2: only works for unit8 type values of RGB.
+def rgba2rgb(rgba, background=(255,255,255)):
     row, col, ch = rgba.shape
 
+	# if already only 3 channel, means it is rgb (or similar format), return as is
     if ch == 3:
         return rgba
 
     assert ch == 4, 'RGBA image has 4 channels.'
-
     rgb = np.zeros( (row, col, 3), dtype='float32' )
     r, g, b, a = rgba[:,:,0], rgba[:,:,1], rgba[:,:,2], rgba[:,:,3]
 
     a = np.asarray( a, dtype='float32' ) / 255.0
-
     R, G, B = background
-
     rgb[:,:,0] = r * a + (1.0 - a) * R
     rgb[:,:,1] = g * a + (1.0 - a) * G
     rgb[:,:,2] = b * a + (1.0 - a) * B
@@ -57,8 +58,11 @@ def now():
 # todo: check if we need to destroy camera class here
 def sigint_handler(sig, frame):
     print('Interrupted')
+    global picam
     if (runner):
         runner.stop()
+    # todo: check if pycam is open before we close it
+    picam.close()
     sys.exit(0)
     
 # exit signal initalized
@@ -85,13 +89,6 @@ def capture_camera_rgb():
 	frame_rgba = capture_camera()
 	frame_rgb = rgba2rgb(frame_rgba)
 	return frame_rgb
-	
-def test(frame_to_write):
-	print('test function')
-	global picam
-	frame_csi_2 = rgba2rgb(frame_to_write)
-	cv2.imwrite("test-python-csi-2.jpg", frame_csi_2)
-	picam.close()
 
 # todo: implement properly
 # right now only shows how to run the python code, nothing else is display
@@ -106,11 +103,11 @@ def azure_client_init():
 
 async def azure_client_send_payload(payload):
 	global azure_client
+	print('sending payload to azure iot hub:', payload)
 	await azure_client.send_message(payload)
 
 # main method
 async def main(argv):
-	print('main started')
 	global picam
 	global azure_client
 
@@ -131,7 +128,7 @@ async def main(argv):
 		help()
 		sys.exit(2)
 
-	# assign model
+	# initialze edge-impulse-related things (get model file into code and init)
 	model = args[0]
 	dir_path = os.path.dirname(os.path.realpath(__file__))
 	modelfile = os.path.join(dir_path, model)
@@ -139,7 +136,6 @@ async def main(argv):
 	
 	# inialize camera
 	camera_init()
-	# initialze edge-impulse-related things
 
 	# initialize azure iot related things
 	azure_client_init()
@@ -149,89 +145,57 @@ async def main(argv):
 			model_info = runner.init()
 			print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
 			labels = model_info['model_parameters']['labels']
-			#if len(args)>= 2:
-				#videoCaptureDeviceId = int(args[1])
-			#else:
-				#port_ids = get_webcams()
-				#if len(port_ids) == 0:
-					#raise Exception('Cannot find any webcams')
-				#if len(args)<= 1 and len(port_ids)> 1:
-					#raise Exception("Multiple cameras found. Add the camera port ID as a second argument to use to this script")
-				#videoCaptureDeviceId = int(port_ids[0])
-			#camera = cv2.VideoCapture(videoCaptureDeviceId)
-			#ret = camera.read()[0]
-			#if ret:
-				#backendName = camera.getBackendName()
-				#w = camera.get(3)
-				#h = camera.get(4)
-				#print("Camera %s (%s x %s) in port %s selected." %(backendName,h,w, videoCaptureDeviceId))
-				#camera.release()
-			#else:
-				#raise Exception("Couldn't initialize selected camera.")
 
+			# todo: change this to make sure we are able to run at optimum fps
 			next_frame = 0 # limit to ~10 fps here
 
-
-
-			#for res, img in runner.classifier(videoCaptureDeviceId):
+			# todo: change this loop to work only when camera is available
 			while True:
+				# get image array from the camera (rgba)
 				img_csi = picam.capture_array()
+				# change that to rgb format
 				img_csi_rgb = rgba2rgb(img_csi)
-
+				# feed to edge impulse methods
 				features, img = runner.get_features_from_image(img_csi_rgb)
 				res = runner.classify(features)
 
+				# tradition delay is induced here
+				# todo: remove/optimize this for best frame rate
 				if (next_frame > now()):
 					time.sleep((next_frame - now()) / 1000)
-
-				print("Result")
-				print(res)
 				
 				# if there is classification in keys()
-				if "classification" in res["result"].keys():
-					print('classification')
-					print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
-					for label in labels:
-						score = res['result']['classification'][label]
-						print('%s: %.2f\t' % (label, score), end='')
-					print('', flush=True)
-
-				elif "bounding_boxes" in res["result"].keys():
-					print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
-					for bb in res["result"]["bounding_boxes"]:
-						print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
-						img = cv2.rectangle(img, (bb['x'], bb['y']), (bb['x'] + bb['width'], bb['y'] + bb['height']), (255, 0, 0), 1)
+				# todo: check if we need all these printed results of if we need this if elif block
+				#if "classification" in res["result"].keys():
+				#	print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
+				#	for label in labels:
+				#		score = res['result']['classification'][label]
+				#		print('%s: %.2f\t' % (label, score), end='')
+				#	print('', flush=True)
+				#elif "bounding_boxes" in res["result"].keys():
+				#	print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
+				#	for bb in res["result"]["bounding_boxes"]:
+				#		print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
+				#		img = cv2.rectangle(img, (bb['x'], bb['y']), (bb['x'] + bb['width'], bb['y'] + bb['height']), (255, 0, 0), 1)
 						
 				if "bounding_boxes" in res["result"].keys():
 					# get the box with the max value, save as top_dog
 					top_dog = res["result"]["bounding_boxes"][0]
-					print("Top Dog")
-					print(top_dog)
-					#PAYLOAD = '{{"X": {x_start}, "Y": {y_start}, "x":{height}, "y":{width}}}'
 					payload = PAYLOAD.format(x_start=top_dog["x"], y_start=top_dog["y"], height=top_dog["height"], width=top_dog["width"])
-					print('Payload')
-					print(payload)
-					print('Sending payload to azure')
 					await azure_client_send_payload(payload)
 						
-
-				if (show_camera):
-					cv2.imshow('edgeimpulse', cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
-					if cv2.waitKey(1) == ord('q'):
-						break
+				# todo: remove this entirely for the demo. We do not even need to spend time to check the if condition every time
+				# this is only for visualization at the edge
+				#if (show_camera):
+				#	cv2.imshow('edgeimpulse', cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+				#	if cv2.waitKey(1) == ord('q'):
+				#		break
 
 				next_frame = now() + 100
+
 		finally:
 			if (runner):
-				runner.stop()
-	
-	# dummy loop right now
-	# todo: create a way for clean exit
-	# while True:
-		#capture_camera()
-		#time.sleep(5)
-	
+				runner.stop()	
 	
 if __name__ == "__main__":
 	asyncio.run(main(sys.argv[1:]))
-	#main(sys.argv[1:])
